68b2b2487d919290c15efffc623b69ec922ea130
fsadh <fsadh@users.noreply.github.com>
2023-11-28 21:38:28 +0300

    Implement CLI argument reading

--- src/bin/driver.rs
+++ src/bin/driver.rs
@@ -1,3 +1,25 @@
+use colored::Colorize;
+use std::{env, process};
+
+const PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
+
 fn main() {
-    print!("Hello, world!\n")
+    let args: Vec<_> = env::args().collect();
+    let mut files: Vec<String> = Vec::new();
+
+    if args.len() == 1 {
+        println!("{} ({})", "scrawlc".green(), PKG_VERSION.bold());
+
+        println!(
+            "{} scrawlc {}",
+            "usage:".yellow().bold(),
+            "[file...]".purple()
+        );
+
+        process::exit(1)
+    } else {
+        for arg in args.iter() {
+            files.push(arg.clone());
+        }
+    }
 }
