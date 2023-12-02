ea76dd0d688ee924d7278f05fb810e68aba86e19
fsadh <fsadh@users.noreply.github.com>
2023-12-02 16:35:36 +0300

    Update binary

--- src/bin.rs
+++ src/bin.rs
@@ -1,4 +1,5 @@
 use colored::Colorize;
+use scrawlc;
 use std::{env, fs, path, process};
 
 const PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -14,8 +15,38 @@ fn print_help() {
     );
 }
 
+fn print_scanner_error(error: &scrawlc::ScannerError) {
+    print!("{} ", "error:".red().bold());
+
+    match error {
+        scrawlc::ScannerError::EndOfContent(position) => {
+            println!(
+                "cannot access {}, end of content",
+                position.to_string().purple()
+            )
+        }
+        scrawlc::ScannerError::UnsupportedCharacter(character) => {
+            println!(
+                "{} is an unsupported character",
+                character.to_string().purple()
+            )
+        }
+    }
+}
+
 fn main() {
     let args: Vec<_> = env::args().collect();
+    let mut verbose: bool = false;
+
+    for arg in args.iter() {
+        if arg == "-v" || arg == "--verbose" {
+            verbose = true;
+        } else if arg == "-h" || arg == "--help" {
+            print_help();
+
+            process::exit(0);
+        }
+    }
 
     if args.len() < 3 {
         print_help();
@@ -23,7 +54,7 @@ fn main() {
         process::exit(1)
     }
 
-    let _input_content = match args.get(1) {
+    let input_content = match args.get(1) {
         Some(file_path) => match fs::read_to_string(file_path) {
             Ok(content) => content,
             Err(error) => {
@@ -74,5 +105,48 @@ fn main() {
         );
 
         process::exit(1)
+    }
+
+    if verbose {
+        print!("  {} Scanning ", "[1/1]".bright_white());
+    }
+
+    let mut scanner = match scrawlc::Scanner::new(&input_content) {
+        Ok(scanner) => scanner,
+        Err(error) => {
+            if verbose {
+                println!("{}", "failed".red());
+            }
+
+            print_scanner_error(&error);
+
+            process::exit(1)
+        }
+    };
+
+    let scan_result = match scanner.scan() {
+        Ok(scan_result) => scan_result,
+        Err(error) => {
+            if verbose {
+                println!("{}", "failed".red());
+            }
+
+            print_scanner_error(&error);
+
+            process::exit(1)
+        }
+    };
+
+    if verbose {
+        println!("{}", "succeeded".green());
+    }
+
+    for (i, token) in scan_result.iter().enumerate() {
+        print!(
+            "    {}{} {}",
+            i.to_string().bright_green(),
+            ":".bright_green(),
+            token
+        );
     }
 }
